0564a2390b3137e363df94bbc7529e91341e144d
HsuPan99 <hsupanthar99@gmail.com>
2020-08-12 18:25:52 +0630

    insert process in item category  manage

---
+++ resources/views/backend/categories/edit.blade.php
@@ -0,0 +1,6 @@
+@extends('backendtemplate')
+@section('content')
+<div class="container-fluid">
+	<h2>Categories Edit(Form with Old Values)</h2>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::get('/', 'BackendController@dashboard')->name('dashboard');
 
 Route::resource('items', 'ItemController'); //7 (get-4/post-1/put-1/delete-1)
 Route::resource('brands', 'BrandController'); //7 (get-4/post-1/put-1/delete-1)
+Route::resource('categories', 'CategoryController'); //7 (get-4/post-1/put-1/delete-1)
 
     
 
