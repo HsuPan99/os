262c9bf79c542e6fc319905d974e676b558752c4
HsuPan99 <hsupanthar99@gmail.com>
2020-08-12 19:06:14 +0630

    insert process in item subcategory  manage

---
+++ resources/views/backend/subcategories/edit.blade.php
@@ -0,0 +1,6 @@
+@extends('backendtemplate')
+@section('content')
+<div class="container-fluid">
+	<h2>Subcategory Edit(Form with Old Values)</h2>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Route::get('/', 'BackendController@dashboard')->name('dashboard');
 Route::resource('items', 'ItemController'); //7 (get-4/post-1/put-1/delete-1)
 Route::resource('brands', 'BrandController'); //7 (get-4/post-1/put-1/delete-1)
 Route::resource('categories', 'CategoryController'); //7 (get-4/post-1/put-1/delete-1)
+Route::resource('subcategories', 'SubcategoryController'); //7 (get-4/post-1/put-1/delete-1)
+
 
     
 
